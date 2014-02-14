c220763222592d5242bd9a64745883f6b463928e
you21979 <you2197901@gmail.com>
2014-02-15 01:35:33 +0900

    add bitpay

---
+++ plugins/in_bitpay.js
@@ -0,0 +1,36 @@
+var agent = null;                                                                                                                            
+var http = null;
+exports.initialize = function(){
+    http = require('http');                                                                                                                  
+    agent = new http.Agent({maxSockets: 1});                                                                                                 
+};
+exports.finalize = function(){
+};
+// api = rates
+exports.run = function(inputTools, callback){
+    var args = inputTools.args();
+    var lists = [];
+    var async = new inputTools.SimpleAsync(function(){
+        callback(null, lists);
+    });
+    var BASE_URL = 'https://bitpay.com/api/';
+    args.api.forEach(function(v){
+        async.inc();
+        try{
+            var url = BASE_URL + v;
+            inputTools.http_get(url, agent, function(err, res){
+                if(err){
+                    console.error(err.stack);
+                }else{
+                    var w = JSON.parse(res);
+                    w.api = v;
+                    lists.push(w);
+                }
+                async.dec();
+            });
+        }catch(err){
+            console.error(err.stack);
+            async.dec();
+        }
+    });
+};
