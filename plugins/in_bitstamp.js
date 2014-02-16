95e45e6aab89165fb2ff2031b17293032741710c
you21979 <you2197901@gmail.com>
2014-02-17 03:36:53 +0900

    add bitstamp

---
+++ plugins/in_bitstamp.js
@@ -0,0 +1,44 @@
+var agent = null;
+var https = null;
+exports.initialize = function(){
+    https = require('https');
+    agent = new https.Agent({maxSockets: 1});
+};
+exports.finalize = function(){
+};
+exports.run = function(inputTools, callback){
+    var args = inputTools.args();
+    var lists = [];
+    var async = new inputTools.SimpleAsync(function(){
+        if(lists.length > 0){
+            callback(null, lists);
+        }else{
+            callback(null, []);
+        }
+    });
+    var BASE_URL = 'https://www.bitstamp.net/api/';
+    args.api.forEach(function(v){
+        var url = BASE_URL + v;
+        async.inc();
+        try{
+            inputTools.https_get(url, agent, function(err, res){
+                if(err){
+                    console.error(err.stack);
+                }else{
+                    try{
+                        var w = JSON.parse(res);
+                        w.api = v;
+                        lists.push(w);
+                    }catch(e){
+                        console.log(res);
+                        console.log(e.stack);
+                    }
+                }
+                async.dec();
+            });
+        }catch(err){
+            console.log(err.stack);
+            async.dec();
+        }
+    });
+};
