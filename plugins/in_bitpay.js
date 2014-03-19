3b6a0856579c7eca1937b20527485b4eaa9294c2
you21979 <you2197901@gmail.com>
2014-03-19 21:15:25 +0900

    refine

--- plugins/in_bitpay.js
+++ plugins/in_bitpay.js
@@ -1,35 +1,25 @@
-var agent = null;                                                                                                                            
-var https = null;
-exports.initialize = function(){
-    https = require('https');                                                                                                                  
-    agent = new https.Agent({maxSockets: 1});                                                                                                 
-};
-exports.finalize = function(){
-};
+exports.initialize = function(){};
+exports.finalize = function(){};
 // api = rates
 exports.run = function(inputTools, callback){
     var args = inputTools.args();
-    var lists = [];
+    var results = [];
+    var crawler = inputTools.crawler;
     var async = new inputTools.SimpleAsync(function(){
-        callback(null, lists);
-    });
-    var BASE_URL = 'https://bitpay.com/api/';
-    args.api.forEach(function(v){
-        async.inc();
-        try{
-            var url = BASE_URL + v;
-            inputTools.https_get(url, agent, function(err, res){
-                if(err){
-                    console.error(err.stack);
-                }else{
-                    var w = JSON.parse(res);
-                    lists.push(w);
-                }
-                async.dec();
-            });
-        }catch(err){
-            console.error(err.stack);
-            async.dec();
+        if(results.length > 0){
+            callback(null, results);
+        }else{
+            callback(null, []);
         }
+    });
+    args.list.forEach(function(v){
+        var url = 'https://bitpay.com/api/' + v;
+        async.inc();
+        crawler.request_get_json(url, function(err, data){
+            if(!err){
+                results.push(data);
+            }
+            async.dec();
+        });
     });
 };
--- plugins/in_bitstamp.js
+++ plugins/in_bitstamp.js
@@ -1,44 +1,25 @@
-var agent = null;
-var https = null;
-exports.initialize = function(){
-    https = require('https');
-    agent = new https.Agent({maxSockets: 1});
-};
-exports.finalize = function(){
-};
+exports.initialize = function(){};
+exports.finalize = function(){};
 exports.run = function(inputTools, callback){
     var args = inputTools.args();
-    var lists = [];
+    var results = [];
+    var crawler = inputTools.crawler;
     var async = new inputTools.SimpleAsync(function(){
-        if(lists.length > 0){
-            callback(null, lists);
+        if(results.length > 0){
+            callback(null, results);
         }else{
             callback(null, []);
         }
     });
-    var BASE_URL = 'https://www.bitstamp.net/api/';
-    args.api.forEach(function(v){
-        var url = BASE_URL + v;
+    args.list.forEach(function(v){
+        var url = 'https://www.bitstamp.net/api/' + v;
         async.inc();
-        try{
-            inputTools.https_get(url, agent, function(err, res){
-                if(err){
-                    console.error(err.stack);
-                }else{
-                    try{
-                        var w = JSON.parse(res);
-                        w.api = v;
-                        lists.push(w);
-                    }catch(e){
-                        console.log(res);
-                        console.log(e.stack);
-                    }
-                }
-                async.dec();
-            });
-        }catch(err){
-            console.log(err.stack);
+        crawler.request_get_json(url, function(err, data){
+            if(!err){
+                data.api = v;
+                results.push(data);
+            }
             async.dec();
-        }
+        });
     });
 };
