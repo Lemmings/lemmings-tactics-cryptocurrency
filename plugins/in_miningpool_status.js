fba8f86a36fd6d36ca3f5e35f710ffcb1fad6ef7
you21979 <you2197901@gmail.com>
2014-03-19 20:59:30 +0900

    move apikey to ini file

---
+++ plugins/in_miningpool_status.js
@@ -0,0 +1,27 @@
+exports.initialize = function(){};
+exports.finalize = function(){};
+exports.run = function(inputTools, callback){
+    var args = inputTools.args();
+    var results = [];
+    var async = new inputTools.SimpleAsync(function(){
+        callback(null, results);
+    });
+    var crawler = inputTools.crawler;
+    var config = inputTools.config;
+    args.list.forEach(function(v){
+        var work = v.split("://");
+        work = work[work.length - 1].split('/');
+        var poolname = work[0];
+        var url = v + '/index.php?page=api&action=getpoolstatus&api_key=' + config[poolname];
+        async.inc();
+        crawler.request_get_json(url, function(err, obj){
+            if(err){
+                console.error(err.stack);
+            }else{
+                obj.getpoolstatus.data.pool_name = poolname;
+                results.push(obj);
+            }
+            async.dec();
+        });
+    });
+};
