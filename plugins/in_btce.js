15e5ecd366f0dc8bdeb9d7c6867211b577a56a93
you21979 <you2197901@gmail.com>
2014-03-19 21:09:51 +0900

    rename

--- plugins/in_btce.js
+++ plugins/in_btce.js
@@ -2,25 +2,22 @@ exports.initialize = function(){};
 exports.finalize = function(){};
 exports.run = function(inputTools, callback){
     var args = inputTools.args();
-    var lists = [];
+    var results = [];
     var crawler = inputTools.crawler;
     var async = new inputTools.SimpleAsync(function(){
-        if(lists.length > 0){
-            callback(null, lists);
+        if(results.length > 0){
+            callback(null, results);
         }else{
             callback(null, []);
         }
     });
-    var BASE_URL = 'https://btc-e.com/api/2/';
-    args.api.forEach(function(v){
-        var url = BASE_URL + v;
+    args.list.forEach(function(v){
+        var url = 'https://btc-e.com/api/2/' + v;
         async.inc();
-        crawler.request_get_json(url, function(err, res){
-            if(err){
-                console.error('%s:%s', url, err.stack);
-            }else{
-                res.api = v;
-                lists.push(res);
+        crawler.request_get_json(url, function(err, data){
+            if(!err){
+                data.api = v;
+                results.push(data);
             }
             async.dec();
         });
