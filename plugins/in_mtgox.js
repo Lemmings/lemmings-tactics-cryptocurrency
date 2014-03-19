2bc8f24eae28968547b03a887ef2f6c70e59acaa
you21979 <you2197901@gmail.com>
2014-03-19 21:17:12 +0900

    refine

--- plugins/in_mtgox.js
+++ plugins/in_mtgox.js
@@ -2,21 +2,22 @@ exports.initialize = function(){};
 exports.finalize = function(){};
 exports.run = function(inputTools, callback){
     var args = inputTools.args();
-    var lists = [];
-    var async = new inputTools.SimpleAsync(function(){
-        callback(null, lists);
-    });
+    var results = [];
     var crawler = inputTools.crawler;
-    var BASE_URL = 'http://data.mtgox.com/api/2/';
-    args.api.forEach(function(v){
-        var url = BASE_URL + v;
+    var async = new inputTools.SimpleAsync(function(){
+        if(results.length > 0){
+            callback(null, results);
+        }else{
+            callback(null, []);
+        }
+    });
+    args.list.forEach(function(v){
+        var url = 'http://data.mtgox.com/api/2/' + v;
         async.inc();
-        crawler.request_get_json(url, function(err, res){
-            if(err){
-                console.error('%s:%s', url, err.stack);
-            }else{
-                res.api = v;
-                lists.push(res);
+        crawler.request_get_json(url, function(err, data){
+            if(!err){
+                data.api = v;
+                results.push(data);
             }
             async.dec();
         });
